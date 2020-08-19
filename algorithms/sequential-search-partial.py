665f60fa3b859fb6f91541e19b403729840c2160
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-08-19 19:32:30 +0200

    feat: add "sequential search" algorithms

---
+++ algorithms/sequential-search-partial.py
@@ -0,0 +1,22 @@
+sequentialvalue = int(input("Enter a (integer) value: "))
+values = [
+  82, 
+  36, 
+  37, 
+  17, 
+  19, 
+  80, 
+  59, 
+  31, 
+  2, 
+  42
+]
+
+found = False
+i = 0
+
+while not found and i < len(values):
+  if values[i] == value:
+    found = True
+
+  i += 1
